052fb6dffe30d2bd207e949914f73ebbaec2bfa2
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-10-19 16:42:31 -0300

    criando estrutura base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component';
+import { LivroDadosComponent } from './livro-dados/livro-dados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivroListaComponent,
+    LivroDadosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/livro-lista/livro-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-lista',
+  templateUrl: './livro-lista.component.html',
+  styleUrls: ['./livro-lista.component.css']
+})
+export class LivroListaComponent {
+
+}
